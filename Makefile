c85f3a549ea93865dfd16f621c19f8ec4fa62e2f
Frédéric Bour <frederic.bour@lakaban.net>
2024-04-10 07:25:41 +0900

    First version that degrades gracefully <3

--- Makefile
+++ Makefile
@@ -1,8 +1,17 @@
-%.o: %.c Makefile
-	gcc -ggdb -O2 -c $<
+OBJECTS=main.o recel_distance.o recel_scan.o stb.o fasttable.o
 
-recel: main.o recel_distance.o recel_scan.o stb.o fasttable.o
+all: build/recel
+
+build/%.o: %.c Makefile
+	gcc -ggdb -O2 -o $@ -c $<
+
+build/recel: $(patsubst %.o,build/%.o, $(OBJECTS))
 	gcc -o $@ -lm $^
 
 clean:
-	rm -rf recel *.o 
+	rm -rf build/*
+
+build/:
+	mkdir $@
+
+.PHONY: all clean
--- main.c
+++ main.c
@@ -1,122 +1,173 @@
 #include <stdlib.h>
+#include <string.h>
 #include "recel.h"
 #include "stb_image.h"
 #include "stb_image_write.h"
 
-uint32_t *interleavev(uint32_t w, uint32_t h, const uint32_t *i1, const uint32_t *i2)
+int inflate_line(const uint32_t *d1, const uint32_t *d2, const uint32_t *i1, const uint32_t *i2, uint32_t *o, int x0, int w)
 {
-  uint32_t *o = NEW_IMAGE(uint32_t, w, h*2-1);
+  int x1 = x0 + 1;
 
-  for (uint32_t y = 0; y < h; ++y)
+  // Length of the line: x1-x0
+  while (x1 < w && d1[x1] < d2[x1-1] && d1[x1-1] < d2[x1])
+    x1 += 1;
+
+  // Classify corners
+  int l = (x0 > 0) ? d1[x0-1] >= d2[x0] : 0;
+  int r = (x1 < w-1) ? d1[x1] >= d2[x1-1] : 0;
+  
+  // Interpolate
+  if (l && r)
   {
-    for (uint32_t x = 0; x < w; ++x)
-    {
-      PIX(o, x, y * 2) = PIX(i1, x, y);
-    }
+    int x = x0;
+    int d = (x1 - x0) / 4;
+
+    for (; x < x0 + d; x++)
+      o[x] = i2[x];
+
+    for (; x < x1 - d; x++)
+      o[x] = i1[x];
+
+    for (; x < x1; x++)
+      o[x] = i2[x];
   }
-  for (uint32_t y = 0; y < h - 1; ++y)
+  else if (l)
   {
-    for (uint32_t x = 0; x < w; ++x)
-    {
-      PIX(o, x, y * 2 + 1) = PIX(i2, x, y);
-    }
+    int x = x0;
+    int d = (x1 - x0) / 2;
+
+    for (; x < x0 + d; x++)
+      o[x] = i2[x];
+
+    for (; x < x1; x++)
+      o[x] = i1[x];
+  }
+  else if (r)
+  {
+    int x = x0;
+    int d = (x1 - x0) / 2;
+
+    for (; x < x0 + d; x++)
+      o[x] = i1[x];
+
+    for (; x < x1; x++)
+      o[x] = i2[x];
+  }
+  else 
+  {
+    int x = x0;
+    int d = (x1 - x0) / 4;
+
+    for (; x < x0 + d; x++)
+      o[x] = i1[x];
+
+    for (; x < x1 - d; x++)
+      o[x] = i2[x];
+
+    for (; x < x1; x++)
+      o[x] = i1[x];
   }
 
-  return o;
+  return x1;
 }
 
-uint32_t *interleaveh(uint32_t w, uint32_t h, const uint32_t *i1, const uint32_t *i2)
+void inflate(const uint32_t *dist, const uint32_t *imag, int w, int h, uint32_t *out)
 {
-  uint32_t *o = NEW_IMAGE(uint32_t, w*2 - 1, h);
-  uint32_t w2 = w * 2 - 1;
-
-  for (uint32_t y = 0; y < h; ++y)
+  for (int y = 0; y < h-1; y++)
   {
-    for (uint32_t x = 0; x < w; ++x)
+    const uint32_t *pd1 = dist + w * (y + 0);
+    const uint32_t *pd2 = dist + w * (y + 1);
+    const uint32_t *pi1 = imag + w * (y + 0);
+    const uint32_t *pi2 = imag + w * (y + 1);
+    uint32_t *po = out + w * y;
+
+    int x = 0;
+    while (x < w)
     {
-      o[x * 2 + y * w2] = PIX(i1, x, y);
+      if (pd1[x] < pd2[x])
+        x = inflate_line(pd1, pd2, pi1, pi2, po, x, w);
+      else if (pd1[x] > pd2[x])
+        x = inflate_line(pd2, pd1, pi2, pi1, po, x, w);
+      else
+      {
+        po[x] = pi1[x];
+        x++;
+        continue;
+      }
     }
   }
-  for (uint32_t y = 0; y < h - 1; ++y)
+}
+
+void interleave(uint32_t *out, const uint32_t *outer, const uint32_t *inner, int w, int h)
+{
+  memcpy(out, outer, sizeof(uint32_t)*w);
+  for (int y = 0; y < h - 1; y++)
   {
-    for (uint32_t x = 0; x < w; ++x)
-    {
-      o[x * 2 + 1 + y * w2] = PIX(i2, x, y);
-    }
+    memcpy(out + w * (2 * y + 1), inner + w * (y + 0), sizeof(uint32_t)*w);
+    memcpy(out + w * (2 * y + 2), outer + w * (y + 1), sizeof(uint32_t)*w);
   }
-
-  return o;
 }
 
-uint32_t *rotate(uint32_t w, uint32_t h, uint32_t *i)
+uint32_t *transpose(const uint32_t *in, int w, int h)
 {
-  uint32_t *o = NEW_IMAGE(uint32_t, h, w);
-  for (int y = 0; y < h; ++y)
-  {
-    for (int x = 0; x < w; ++x)
-    {
-      o[y + x * h] = i[x + y * w];
-    }
-  }
-
-  return o;
-}
-
-void scanv(uint32_t w, uint32_t h, uint32_t *i, uint32_t *id, uint32_t **o, uint32_t **od)
-{
-  *o = NEW_IMAGE(uint32_t, w, h);
-  *od = NEW_IMAGE(uint32_t, w, h);
-  recel_scan(w, h, id, i, *od, *o);
-}
-
-void scanh(uint32_t w, uint32_t h, uint32_t *i, uint32_t *id, uint32_t **o, uint32_t **od)
-{
-  i = rotate(w, h, i);
-  id = rotate(w, h, id);
-  uint32_t *t, *td;
-  scanv(h, w, i, id, &t, &td);
-  *o = rotate(h, w, t);
-  *od = rotate(h, w, td);
-  free(t);
-  free(td);
-  free(i);
-  free(id);
+  uint32_t *result = NEW_IMAGE(uint32_t, h, w);
+  for (int y = 0; y < h; y++)
+    for (int x = 0; x < w; x++)
+      result[x * h + y] = in[y * w + x];
+  return result;
 }
 
 int main(int argc, char **argv)
 {
   int w, h, n;
-  void *input = stbi_load(argv[1], &w, &h, &n, 4);
+  uint32_t *imag, *dist, *disti, *imagi, *distii, *imagii;
+
+  imag = (uint32_t*)stbi_load(argv[1], &w, &h, &n, 4);
   printf("loaded '%s', %d*%d*%d\n", argv[1], w, h, n);
 
-  uint32_t *dist = recel_distance(w, h, input);
+  dist = recel_distance(w, h, imag);
   recel_save_dist("dist.png", w, h, dist);
 
-  uint32_t *horz, *horzd, *vert, *vertd;
-  scanv(w, h, input, dist, &vert, &vertd);
+  for (int i = 0; i < 2; i++)
+  {
+    disti = NEW_IMAGE(uint32_t, w, h-1);
+    imagi = NEW_IMAGE(uint32_t, w, h-1);
+    inflate(dist, dist, w, h, disti);
+    inflate(dist, imag, w, h, imagi);
 
-  stbi_write_png("vert.png", w, h*2-1, 4, interleavev(w, h, input, vert), 0);
-  recel_save_dist("vert_dist.png", w, h*2-1, interleavev(w, h, dist, vertd));
+    distii = NEW_IMAGE(uint32_t, w, 2*h-1);
+    imagii = NEW_IMAGE(uint32_t, w, 2*h-1);
+    interleave(distii, dist, disti, w, h);
+    interleave(imagii, imag, imagi, w, h);
+    h = h * 2 - 1;
 
-  uint32_t *ih, *ihd;
-  scanh(w, h, input, dist, &horz, &horzd);
-  ih = interleaveh(w, h, input, horz);
-  ihd = interleaveh(w, h, dist, horzd);
-  free(horz);
-  free(horzd);
+    free(imag);
+    free(dist);
+    free(disti);
+    free(imagi);
 
-  stbi_write_png("horz.png", w*2-1, h, 4, ih, 0);
-  recel_save_dist("horz_dist.png", w*2-1, h, ihd);
+    char fdist[] = "dist-_.png";
+    char fimag[] = "imag-_.png";
+    fdist[5] = fimag[5] = '0' + i;
 
-  scanv(w*2-1, h, ih, ihd, &horz, &horzd);
-  vert = interleavev(w*2-1, h, ih, horz);
-  vertd = interleavev(w*2-1, h, ihd, horzd);
+    stbi_write_png(fimag, w, h, 4, imagii, 0);
+    recel_save_dist(fdist, w, h, distii);
 
-  stbi_write_png("out.png", w*2-1, h*2-1, 4, vert, 0);
-  recel_save_dist("out_dist.png", w*2-1, h*2-1, vertd);
+    imag = transpose(imagii, w, h);
+    dist = transpose(distii, w, h);
+    free(imagii);
+    free(distii);
+
+    int t = w;
+    w = h;
+    h = t;
+  }
+
+  stbi_write_png("outi.png", w, h, 4, imag, 0);
+  recel_save_dist("outd.png", w, h, dist);
 
   free(dist);
-  free(input);
+  free(imag);
 
   return 0;
 }
